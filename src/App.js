5715a93b6dd574ef3741f50c753ee13cf839e699
wongtawi-18 <wongtawiaut2@gmail.com>
2019-09-22 23:20:30 +0700

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,93 +1,12 @@
-import React from 'react';
-import CharacterCard from './CharacterCard';
-import _ from 'lodash';
-//import WordCard from './WordCard';
-import './App.css'
-
-let message = 'Hello'
-
-//const fn = () => {
-
-//}
-
-const prepareStateFromWord = (given_word) => {
-  let word = given_word.toUpperCase()
-  let chars = _.shuffle(Array.from(word))
-  return {
-    word,
-    chars,
-    attempt: 1,
-    guess: [],
-    completed: false,
-    reset: 1
-  }
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+ render() {
+ return (
+ <div className="App">
+ Hello World
+ </div>
+ );
+ }
 }
-
-
-class App extends React.Component {
-
-  state = prepareStateFromWord(message);
-
-  reset = () => {
-    this.setState({
-      reset: this.state.reset + 1,
-      completed: !this.state.completed
-    })
-  }
-  activationHandler = (c) => {
-    let guess = [...this.state.guess, c]
-    this.setState({ guess })
-    if (guess.length == this.state.chars.length) {
-      if (guess.join('').toString() == this.state.word) {
-        this.setState({ guess: [], completed: true })
-      } else {
-        this.setState({ guess: [], attempt: this.state.attempt + 1 })
-      }
-    }
-  }
-
-
-  render() {
-    return (
-      <div>
-        {
-          Array.from(this.state.chars).map((item, index) =>
-            <CharacterCard value={item} key={index}
-              attempt={this.state.attempt}
-              activationHandler={this.activationHandler}
-              reset={this.state.reset} />
-
-          )
-        }
-        <h2>Selected</h2>
-        {
-          Array.from(this.state.guess).map((item, index) => (
-            <CharacterCard
-              value={item}
-              key={index}
-              activationHandler={this.activationHandler}
-            />
-          ))
-        }
-        <div>Attemp {this.state.attempt}</div>
-        {
-          this.state.completed && <h4>Complete</h4>
-        }
-
-        {
-          this.state.completed && <button onClick={this.reset}>Complete reset</button>
-        }
-
-      </div>
-
-    )
-
-
-  }
-
-
-
-}
-
-
 export default App;

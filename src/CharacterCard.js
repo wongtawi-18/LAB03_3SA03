94f32882cf1ef9de27696a11c6ed10144ccfe4ca
wongtawi-18 <wongtawiaut2@gmail.com>
2019-09-22 23:43:37 +0700

    Ten commit

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,7 +1,24 @@
 export default class CharacterCard extends Component {
-    render(){
-    return (
-    <div>{this.props.value}</div>
-    )
+    
+   export default class CharacterCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            active: false,
+        }
     }
-   }
+
+    activate = () => {
+        if (!this.state.active) {
+            this.setState({ active: true, seq })
+        }
+    }
+    render() {
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+        return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
+    }
+}

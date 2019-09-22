ae879e0089b71118e1a226ed1bb482323f32640d
wongtawi-18 <wongtawiaut2@gmail.com>
2019-09-22 23:22:23 +0700

    Four commit

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,39 +1,7 @@
-import React from 'react';
-
-
-class CharacterCard extends React.Component {
-    state = {
-        active: false
+export default class CharacterCard extends Component {
+    render(){
+    return (
+    <div>{this.props.value}</div>
+    )
     }
-
-    activate = () => {
-        this.setState({
-            active: true
-        });
-
-        if (this.state.active === false)
-            this.props.activationHandler(this.props.value);
-    }
-    componentDidUpdate = (prevProps) => {
-        console.log(this.props.attempt);
-        if (prevProps.attempt !== this.props.attempt || prevProps.reset !==this.props.reset) 
-        {
-            this.setState({ active: false })
-            console.log('...');
-        }
-    }
-    render() {
-        //  let activateClass = this.state.act
-        let className = `card ${this.state.active ? 'activeCard' : ''}`
-        return (
-            <div className={className} onClick={this.activate}>
-
-                {this.props.value}
-            </div>
-
-        )
-    }
-}
-
-
-export default CharacterCard;
+   }

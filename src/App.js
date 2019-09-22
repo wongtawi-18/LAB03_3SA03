71348c2c15da74e46e19308cfbd69982dc7d5884
wongtawi-18 <wongtawiaut2@gmail.com>
2019-09-22 23:24:09 +0700

    Five commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
 class App extends Component {
- render() {
- return (
- <div className="App">
- Hello World
- </div>
- );
+  render() {
+  return (
+  <div>
+  <CharacterCard value="h"/>
+ <CharacterCard value="i"/>
+  </div>
+  );
+  }
  }
-}
 export default App;
